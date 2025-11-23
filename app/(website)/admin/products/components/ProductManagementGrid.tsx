c058f0bd90a0daa3a547f6019afec951844d9a1d
CDS Alex <cds.MoatazEhab98262@alexu.edu.eg>
2025-11-23 18:09:51 +0200

    4 products

--- app/(website)/admin/products/components/ProductManagementGrid.tsx
+++ app/(website)/admin/products/components/ProductManagementGrid.tsx
@@ -93,7 +93,7 @@ export default function ProductManagementGrid({
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
         {products.map((product) => (
           <ProductCard
             key={product.id}
